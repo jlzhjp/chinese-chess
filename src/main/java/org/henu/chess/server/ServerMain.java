4bfaee1442e93503181af70ff77d7c923307457b
ZJ Jiang <jvjdev@outlook.com>
2023-06-03 22:25:29 +0800

    remove server

--- src/main/java/org/henu/chess/server/ServerMain.java
+++ src/main/java/org/henu/chess/server/ServerMain.java
@@ -1,14 +1,9 @@
 package org.henu.chess.server;
 
 import com.formdev.flatlaf.FlatLightLaf;
-import org.henu.chess.server.controller.ServerViewController;
-import org.henu.chess.server.view.ServerWindow;
 
 public class ServerMain {
     public static void main(String[] args) {
         FlatLightLaf.setup();
-        ServerWindow window = new ServerWindow();
-        ServerViewController controller = new ServerViewController(window);
-        window.show();
     }
 }
